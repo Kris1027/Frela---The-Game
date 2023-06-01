f5dce6aa4ed609552140d221555d62c6cb4d3422
Krzysztof Obarzanek <krzys89@gmail.com>
2023-06-01 12:51:39 +0200

    add player moving

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const player = document.querySelector('.player');
+const playerElement = document.querySelector('.player');
+const gameArea = document.querySelector('.container');
 
-let movingSpeed = 2;
+const movePlayer = (direction) => {
+    // new position of the player
+    const newPosition = playerElement.offsetLeft + direction * 20;
+    // get position of the game area
+    const {left, right} = gameArea.getBoundingClientRect();
+    // size of the game area
+    const minLeft = playerElement.offsetWidth;
+    const maxRight = right - left - minLeft;
+    // if player is inside the game area - move player
+    if (newPosition >= 0 && newPosition < maxRight) {
+        playerElement.style.left = `${newPosition}px`;
+    }
+};
 
-const playerMove = (e) => {
-
+const handleKeyboard = (e) => {
+    // console.log(e.keyCode);
+    switch (e.code) {
+        case 'ArrowLeft': movePlayer(-1); break;
+        case 'ArrowRight': movePlayer(1); break;
+    }
 }
 
-player.addEventListener('keydown', playerMove);
+window.addEventListener('keydown', handleKeyboard);
